78f87d7177cb63687d558b7c5ec709b9717e0fbc
yuito118 <72675978+yuito118@users.noreply.github.com>
2023-01-29 16:48:28 +0900

    update: 文言

--- top.py
+++ top.py
@@ -5,8 +5,6 @@ st.title('Top')
 st.markdown('''
 - [ケモインフォマティクスとは](https://speakerdeck.com/yuito118/kemoinhuomateikusu)
 
-- [データ](https://docs.google.com/spreadsheets/d/1wVIW3WQa5yfssVWcHpbZzWS2kdH7gVMy0B5yXw6Qd88)
-
 ## 注意
 1. 欠損値は基本消える
 ''')
