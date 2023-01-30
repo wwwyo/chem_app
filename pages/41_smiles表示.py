10b0da9aece8fb23f370cb82fc3e4063f69afa9c
yuito118 <72675978+yuito118@users.noreply.github.com>
2023-01-30 15:50:13 +0900

    add: smiles to img

---
+++ pages/41_smiles表示.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import sys
+import streamlit as st
+import pandas as pd
+# 親のmodule群をimportできるように
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from components.DownloadLink import DownloadLink
+from components import Divider
+from rdkit import Chem
+from rdkit.Chem import Draw
+
+st.title('smiles表示')
+
+st.subheader('1. データ アップロード')
+csv = st.file_uploader(label='csv', type='csv', label_visibility='hidden', key='csv')
+df = pd.DataFrame()
+
+if (csv):
+    df = pd.read_csv(csv)
+    st.write(f'csvのサイズ: {df.shape}')
+    st.dataframe(df)
+
+    st.subheader('2. smilesを選択')
+    smiles_col = st.selectbox(
+        key='smiles_col',
+        label='smiles_col',
+        label_visibility='hidden',
+        options=(df.columns)
+    )
+
+    for smiles in df[smiles_col]:
+        mol = Chem.MolFromSmiles(smiles)
+        img = Draw.MolToImage(mol)
+        st.image(img, caption=smiles,use_column_width=True)
