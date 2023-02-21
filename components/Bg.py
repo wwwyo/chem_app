e9e2d6728437fc4fa30e5920b394eee2d0f8678b
yuito118 <72675978+yuito118@users.noreply.github.com>
2023-02-22 03:29:31 +0900

    update: top background

---
+++ components/Bg.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import base64
+
+def render()->st:
+    bg_img = '''
+    <style>
+    .stApp {
+      background-image: url("https://drive.google.com/uc?export=view&id=1eG0HtX4tDht4U9fxPLe52tnxK9bfX_cM"),linear-gradient(to bottom, rgba(25,26,42,1) 30%, rgba(18,16,23,1));
+      background-size: cover;
+      background-repeat: no-repeat;
+      opacity: 0.85;
+    }
+    </style>
+    '''
+
+    return st.markdown(bg_img, unsafe_allow_html=True)
--- top.py
+++ top.py
@@ -1,10 +1,16 @@
 import streamlit as st
+from components import Bg
 
-
-st.title('Top')
+Bg.render()
 st.markdown('''
-- [ケモインフォマティクスとは](https://speakerdeck.com/yuito118/kemoinhuomateikusu)
+<h1 style='font-size: 64px; font-family:serif; text-align: right;margin-top:84px;'>Chemoinfo</h1>
 
-## 注意
-1. 欠損値は基本消える
-''')
+<div style='font-size: 12px;width: 28rem; text-align:right;margin: auto 0 auto auto'>Introducing our new chemoinformatics app - the ultimate tool for analyzing chemical data. Streamline your research with advanced algorithms and customizable features. Get the insights you need, faster than ever before. Try it today!</div>
+
+<p style='font-size: 34px; text-align: right;margin-top:64px;color:gold;line-height:1.3;font-family:serif;'>Begin experimenting <br/>quickly and easily.</p>
+<style>
+.block-container {
+  max-width: 100vw !important;
+}
+</style>
+''', unsafe_allow_html=True)
