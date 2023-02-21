385e88c2b9b2e4c4997a01ef278f289d61776db5
yuito118 <72675978+yuito118@users.noreply.github.com>
2023-02-22 06:50:46 +0900

    style: font color

--- top.py
+++ top.py
@@ -3,9 +3,9 @@ from components import Bg
 
 Bg.render()
 st.markdown('''
-<h1 style='font-size: 64px; font-family:serif; text-align: right;margin-top:84px;'>Chemoinfo</h1>
+<h1 style='font-size: 64px; font-family:serif; text-align: right;margin-top:84px; color:white;'>Chemoinfo</h1>
 
-<div style='font-size: 12px;width: 28rem; text-align:right;margin: auto 0 auto auto'>Introducing our new chemoinformatics app - the ultimate tool for analyzing chemical data. Streamline your research with advanced algorithms and customizable features. Get the insights you need, faster than ever before. Try it today!</div>
+<div style='font-size: 12px;width: 28rem; text-align:right;margin: auto 0 auto auto; color:white;'>Introducing our new chemoinformatics app - the ultimate tool for analyzing chemical data. Streamline your research with advanced algorithms and customizable features. Get the insights you need, faster than ever before. Try it today!</div>
 
 <p style='font-size: 34px; text-align: right;margin-top:64px;color:gold;line-height:1.3;font-family:serif;'>Begin experimenting <br/>quickly and easily.</p>
 <style>
